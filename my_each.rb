0c6292bc17480c68d63fa71fb25ee7c9db0b2ad1
Eugene White <eugenewhite1994@gmail.com>
2020-07-14 02:31:26 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,5 +1,8 @@
-def my_each(collections)
-  collection = [1,2,3,4]
-  while collection < 5
-  yield(collection)
+def my_each(arr)
+  i = 0
+  while i < arr.length
+    yield(arr[i])
+    i += 1
+  end
+  arr
 end
